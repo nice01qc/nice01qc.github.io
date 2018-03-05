d76c3c6d6a76d40a6f2917e66329981e043347a4
ljh <734522310@qq.com>
2018-03-05 22:05:15 +0800

    upgrade

---
+++ assets/js/picture.js
@@ -0,0 +1,80 @@
+var picture=document.getElementById('picture');
+var numli=document.getElementById('num').getElementsByTagName('li');
+  var timeout=timeout1=null;
+var pictureli=picture.getElementsByTagName('li');
+var imgarr=new Array;
+for(var i=0;i<pictureli.length;i++){
+  imgarr.push(pictureli[i].offsetTop);
+}
+for(var i=0;i<numli.length;i++){
+  numli[i].index=i;
+  numli[i].onclick=function(){
+    for(var j=0;j<pictureli.length;j++){
+      numli[j].style.background="white";
+    }
+    this.style.background="#9D9D9D";
+    clearInterval(timeout1);
+    show(this.index);
+    if(this.index==(numli.length-1)){
+      turns(this.index-1);
+    }else {
+      turns(this.index);
+    }
+  };
+}
+turns(1);
+
+function turns(index){
+  var number=index;
+  var pan1=0;
+  timeout1=setInterval(function(){
+    for(var j=0;j<pictureli.length;j++){
+      numli[j].style.background="white";
+    }
+    numli[number].style.background="#9D9D9D";
+    show(number);
+    if(pan1==0){
+      number++;
+    }
+    if(number>=pictureli.length){
+      pan1=1;
+      number--;
+    }
+    if(pan1==1){
+      number--;
+    }
+    if(number<0){
+      pan1=0;
+      number=1;
+    }
+  },2000);
+}
+
+function show(index){
+clearInterval(timeout);
+var height=-imgarr[index];
+var x=picture.offsetTop;
+timeout=setInterval(function(){
+    if(x>height){
+      if(x>height+300){
+        x-=30;
+      }else if(x<=height+300 && x>height+60){
+        x-=4;
+      }else if(x<=height+60){
+        x--;
+      }
+      if(x<height)x=height;
+    }else {
+      if(x<height-300){
+        x+=30;
+      }else if(x>=height-300&&x<height-60){
+        x+=4;
+      }else if(x>=height-60){
+        x++;
+      }
+      if(x>height)x=height;
+    }
+    picture.style.top=x+"px";
+    if(x==height){clearInterval(timeout);}
+},2);
+}
